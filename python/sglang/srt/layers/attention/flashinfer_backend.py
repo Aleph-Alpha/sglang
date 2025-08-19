f861054d49d21765195799af99cc16aaf253e321
Eric Schreiber <eric.schreiber@aleph-alpha.com>
2025-08-19 10:27:32 +0200

    Re-introduce the initial state but break before any illegal memory access can happen to help with debugging.

--- python/sglang/srt/layers/attention/flashinfer_backend.py
+++ python/sglang/srt/layers/attention/flashinfer_backend.py
@@ -754,14 +754,10 @@ class FlashInferIndicesUpdaterDecode:
     ):
         if spec_info is None:
             bs = len(req_pool_indices)
-            if seq_lens_cpu is None:
-                kv_indptr[1 : bs + 1] = torch.cumsum(paged_kernel_lens, dim=0)
-            else:
-                # Computes cumsum on CPU and copy to GPU to avoid GPU-side errors
-                kv_indptr_cpu = torch.empty((bs + 1,), dtype=torch.int32, device="cpu")
-                kv_indptr_cpu[0] = 0
-                torch.cumsum(seq_lens_cpu, dim=0, out=kv_indptr_cpu[1:])
-                kv_indptr[: bs + 1].copy_(kv_indptr_cpu, non_blocking=True)
+            assert (
+                seq_lens_cpu is None
+            )  # For now we break to make sure not to run into ECC memory issues.
+            kv_indptr[1 : bs + 1] = torch.cumsum(paged_kernel_lens, dim=0)
             kv_indptr = kv_indptr[: bs + 1]
 
             if wrapper.is_cuda_graph_enabled:
